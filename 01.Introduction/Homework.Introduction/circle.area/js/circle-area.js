c738e132c77009df0f3b62a8aa81f4b64f82b1a2
ivayloivanof <ivaylo.ivanof@gmail.com>
2016-01-07 23:25:09 +0200

    decide problem 2 - circle area

--- 01.Introduction/Homework.Introduction/circle.area/js/circle-area.js
+++ 01.Introduction/Homework.Introduction/circle.area/js/circle-area.js
@@ -1,3 +1,6 @@
-window.onload = function() {
-
+function calcCircleArea() {
+	var p = 3.1415926535897932384626433832795028841971693993751058209749445;
+	var radius = document.area.radius.value;
+	var result = p * (radius * radius);
+	document.getElementById("circle-area").innerHTML = "r = " + radius + "; area = " + result.toPrecision(17);
 }
